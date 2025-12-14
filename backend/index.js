99dc14daf386d621815eb06b6ebe3ed9271b400a
JayanthThota07 <venkatajayanththota@gmail.com>
2025-12-14 08:09:09 +0530

    added target value

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,10 @@ import express from "express";
 import { createTodo, updateTodo } from "./types.js";
 import { Todo } from "./db.js";
 import cors from "cors";
-
 import dotenv from "dotenv";
-
 dotenv.config();
-app.use(cors());
-
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // CREATE TODO
